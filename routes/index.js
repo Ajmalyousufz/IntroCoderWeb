4f7079e9cb6bc6a94d3b214895fd1b75afffccac
Muhammed Ajmal y <Ajmalyousufza@gmail.com>
2023-01-09 21:04:06 +0530

    json added in views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/introjson', function(req, res, next) {
+  res.render('/jsons/intro_coder_json');
+});
+
 module.exports = router;
